70e195fcde2f1e693427f4eec6214d55db6fdd9d
Jatinnarang22 <njatin3435@gmail.com>
2023-04-19 23:12:42 +0530

    done with login ans signup pages

--- app.js
+++ app.js
@@ -1,32 +1,39 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-let path = require('path');
-const mongoose = require('mongoose');
-let seedDB = require('./seed');
-let ejsMate = require('ejs-mate');
-let productRoutes = require('./routes/products/productsRoutes');
-let revRoutes = require('./routes/review');
-let methodOverride =  require('method-override');
+let path = require("path");
+const mongoose = require("mongoose");
+let seedDB = require("./seed");
+let ejsMate = require("ejs-mate");
+let auth = require("./routes/auth");
+let productRoutes = require("./routes/products/productsRoutes");
+let revRoutes = require("./routes/review");
+let methodOverride = require("method-override");
+let cookieParser = require("cookie-parser");
 
-mongoose.set('strictQuery' , true);
-mongoose.connect('mongodb://127.0.0.1:27017/college')
-.then(()=>{console.log("DB connected")})
-.catch((err)=>{console.log(err)})
+mongoose.set("strictQuery", true);
+mongoose
+  .connect("mongodb://127.0.0.1:27017/college")
+  .then(() => {
+    console.log("DB connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
-
-
-app.use(methodOverride('_method'));
-app.engine('ejs' , ejsMate);
-app.set('views' , path.join(__dirname , 'views'));
-app.set('view engine' , 'ejs');
-app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname , 'public'))); //static files
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); //static files
+app.use(cookieParser());
+app.use(auth);
 app.use(productRoutes);
 app.use(revRoutes);
+
 // seedDB();
 
-
 let port = 8000;
-app.listen(port , ()=>{
-    console.log(`server connected at port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`server connected at port ${port}`);
+});
--- routes/products/productsRoutes.js
+++ routes/products/productsRoutes.js
@@ -1,64 +1,68 @@
-let express = require('express');
-const Product = require('../../models/product');
+let express = require("express");
+const Product = require("../../models/product");
+const User = require("../../models/User");
 let router = express.Router();
+const bcrypt = require("bcrypt");
 
+router.get("/products", async (req, res) => {
+  if (req.cookies.user_id) {
+    let userid = User.findOne(req.cookies.user_id);
+    if (userid) {
+      let products = await Product.find();
+      res.render("product/index", { products });
+    } else {
+      return res.redirect("./Signin");
+    }
+  } else {
+    return res.redirect("./Signin");
+  }
+});
 
-    router.get('/products' , async(req,res)=>{
-        let products = await Product.find();
-        res.render('product/index' , {products} )
-    })
- 
-    //for new new itnes 
-    router.get('/products/new' , (req,res)=>{
-        res.render('product/new');
-    });
+//for new new itnes
+router.get("/products/new", (req, res) => {
+  res.render("product/new");
+});
 
+// to view the product detail
+// router.get('/products/:id',async(req,res)=>{
+//     let {id}=req.params;
+//     console.log("mil gayaa");
+//     // console.lof(res.prompt._id);
+//     let findid =  await Product.findById(id).populate('reviews');
+//     // console.log(findid.reviews);
+//     res.render('product/show',{findid});
+// });
 
-    // to view the product detail
-    // router.get('/products/:id',async(req,res)=>{
-    //     let {id}=req.params;
-    //     console.log("mil gayaa"); 
-    //     // console.lof(res.prompt._id);
-    //     let findid =  await Product.findById(id).populate('reviews');
-    //     // console.log(findid.reviews);
-    //     res.render('product/show',{findid});
-    // });
+// to display a particular product
+router.get("/products/:id", async (req, res) => {
+  let { id } = req.params;
+  let product = await Product.findById(id);
+  let findid = await Product.findById(id).populate("reviews");
+  res.render("product/show", { findid });
+});
 
-      // to display a particular product
-      router.get('/products/:id' , async(req,res)=>{
-        let {id} =req.params;
-        let product = await Product.findById(id);
-        let findid = await Product.findById(id).populate('reviews');
-        res.render('product/show' , {findid});
-    });
+router.post("/products", async (req, res) => {
+  let { name, img, price, desc } = req.body;
+  await Product.create({ name, img, price, desc });
+  res.redirect("/products");
+});
 
-    router.post('/products' , async(req,res)=>{
-        let {name , img, price , desc} = req.body;
-        await Product.create({name , img, price , desc});
-        res.redirect('/products');
-    });
+router.get("/products/:id/edit", async (req, res) => {
+  let { id } = req.params;
+  let foundProduct = await Product.findById(id);
+  res.render("product/update", { foundProduct });
+});
 
+router.patch("/products/:id", async (req, res) => {
+  let { id } = req.params;
+  let { name, img, price, desc } = req.body;
+  await Product.findOneAndUpdate(id, { name, img, price, desc });
+  res.redirect("/products");
+});
 
-    
-
-    router.get('/products/:id/edit',async(req,res)=>{
-        let {id} =req.params;
-        let foundProduct = await Product.findById(id);
-        res.render('product/update',{foundProduct});
-    });
-
-    router.patch('/products/:id',async(req,res)=>{
-            let {id} =req.params;
-            let {name,img,price,desc} = req.body;
-            await Product.findOneAndUpdate(id,{name,img,price,desc});
-            res.redirect('/products');
-    });
-
-  
-
-    router.delete ('/products/:id',async(req,res)=>{
-        let {id} =req.params;
-        await Product.findOneAndDelete(id);
-        res.redirect('/products');
-    });
-module.exports = router
+router.delete("/products/:id", async (req, res) => {
+  let { id } = req.params;
+  await Product.findOneAndDelete(id);
+  res.redirect("/products");
+});
+module.exports = router;
